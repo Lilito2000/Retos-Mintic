5b1e6d843834e923de000af26354d9de521fe35b
NicolasMisionT <nicolas.caicedo.mt@correo.usa.edu.co>
2021-10-10 23:18:19 -0500

    creacion de tabla category

--- reto3/src/main/java/com/usa/ciclo3/ciclo3/repository/crud/CategoriesCrudRepository.java
+++
@@ -1,9 +0,0 @@
-package com.usa.ciclo3.ciclo3.repository.crud;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.usa.ciclo3.ciclo3.model.Categories;
-
-public interface CategoriesCrudRepository extends CrudRepository<Categories, Integer> {
-
-}
--- reto3/src/main/java/com/usa/ciclo3/ciclo3/repository/crud/ReservationsCrudRepository.java
+++
@@ -1,9 +0,0 @@
-package com.usa.ciclo3.ciclo3.repository.crud;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.usa.ciclo3.ciclo3.model.Reservations;
-
-public interface ReservationsCrudRepository extends CrudRepository<Reservations, Integer> {
-
-}
