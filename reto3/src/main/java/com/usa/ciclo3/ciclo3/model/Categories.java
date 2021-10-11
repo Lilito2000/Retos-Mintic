be1968ca74ea01d3c53fbf9f3417ed6e60feeb28
NicolasMisionT <nicolas.caicedo.mt@correo.usa.edu.co>
2021-10-10 23:08:58 -0500

    creacion de tabla category

---
+++ reto3/src/main/java/com/usa/ciclo3/ciclo3/model/Categories.java
@@ -0,0 +1,37 @@
+package com.usa.ciclo3.ciclo3.model;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table(name="categories")
+public class Categories implements Serializable {
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private Integer idCategories;
+
+    @Column(name="name", nullable=true, length= 45)
+    private String name;
+
+    @Column(name="description", nullable=true, length= 250)
+    private String description;
+
+    public Integer getIdCategories() {
+        return idCategories;
+    }
+    public void setIdCategories(Integer idReservation) {
+        this.idCategories = idReservation;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
