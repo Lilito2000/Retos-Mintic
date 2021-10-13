f845f1d17b83b7110ca19252bf8a23c5b5ea6fce
David Ballen Vargas <dhballen@gmail.com>
2021-10-12 20:54:42 -0500

    se integran Skate, Categories, Reservations

--- proyectoCiclo3/src/main/java/com/usa/ciclo3/ciclo3/repository/crud/CategoryCrudRepository.java
+++
@@ -1,8 +0,0 @@
-package com.usa.ciclo3.ciclo3.repository.crud;
-
-import com.usa.ciclo3.ciclo3.model.Category;
-import com.usa.ciclo3.ciclo3.model.Skateboard;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoryCrudRepository extends JpaRepository<Category, Integer> {
-}
