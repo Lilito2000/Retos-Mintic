4c7833b62ef8174da8ddded7533db7255f4f6f14
David Ballen Vargas <dhballen@gmail.com>
2021-10-13 23:47:21 -0500

    fix bugs reservations and Message

--- src/main/java/com/usa/ciclo3/ciclo3/model/Message.java
+++ src/main/java/com/usa/ciclo3/ciclo3/model/Message.java
@@ -7,36 +7,48 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
-/**
- *
- * @author lilia
- */
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 @Entity
-@Table(name = "message")
+@Table(name = "Message")
 public class Message implements Serializable {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Integer idMessage; // debe ser invisible
-	
+	private Integer idMessage;
 	@Column(length = 250)
-	private String messageText;// no superior a 250 caracteres
+	private String messageText;
 
-	
+	@ManyToOne
+	@JoinColumn(name = "clientId")
+	@JsonIgnoreProperties("messages")
+	private Client client;
+
 	public Integer getIdMessage() {
 		return idMessage;
 	}
+
 	public void setIdMessage(Integer idMessage) {
 		this.idMessage = idMessage;
 	}
+
 	public String getMessageText() {
 		return messageText;
 	}
+
 	public void setMessageText(String messageText) {
 		this.messageText = messageText;
 	}
-	
-	
+
+	public Client getClient() {
+		return client;
+	}
+
+	public void setClient(Client client) {
+		this.client = client;
+	}
 }
